8ac256994169fbaab92bed7408839e13c7980b6c
Jesper Beisner <jesper.beisner+github@gmail.com>
2023-08-14 07:41:04 +0200

    update renderer to use self invoking function where vars are extracted

--- src/Helper/Renderer.php
+++ src/Helper/Renderer.php
@@ -18,28 +18,33 @@ final readonly class Renderer
         $templateFile = Renderer::VIEWS_DIRECTORY . '/' . $template;
         $layoutFile = Renderer::VIEWS_DIRECTORY . '/layout/layout.phtml';
 
-        if (!is_file($templateFile)) {
-            throw new RuntimeException(sprintf('Template file "%s" does not exist.', $templateFile));
-        }
+        return (function (string $templateFile, string $layoutFile, array $vars, bool $withLayout): string {
+            extract($vars);
+            unset($vars);
 
-        ob_start();
-        require $templateFile;
-        if (false === $content = ob_get_clean()) {
-            throw new RuntimeException(sprintf('"ob_get_clean" returned false for template file "%s".', $templateFile));
-        }
-
-        if ($withLayout === true) {
-            if (!is_file($layoutFile)) {
-                throw new RuntimeException(sprintf('Layout file "%s" does not exist.', $layoutFile));
+            if (!is_file($templateFile)) {
+                throw new RuntimeException(sprintf('Template file "%s" does not exist.', $templateFile));
             }
 
             ob_start();
-            require $layoutFile;
+            require $templateFile;
             if (false === $content = ob_get_clean()) {
-                throw new RuntimeException(sprintf('"ob_get_clean" returned false for layout file "%s".', $layoutFile));
+                throw new RuntimeException(sprintf('"ob_get_clean" returned false for template file "%s".', $templateFile));
             }
-        }
 
-        return $content;
+            if ($withLayout === true) {
+                if (!is_file($layoutFile)) {
+                    throw new RuntimeException(sprintf('Layout file "%s" does not exist.', $layoutFile));
+                }
+
+                ob_start();
+                require $layoutFile;
+                if (false === $content = ob_get_clean()) {
+                    throw new RuntimeException(sprintf('"ob_get_clean" returned false for layout file "%s".', $layoutFile));
+                }
+            }
+
+            return $content;
+        })($templateFile, $layoutFile, $vars, $withLayout);
     }
 }
